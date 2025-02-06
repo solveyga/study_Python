e486c38c2287db000e2907c67716c0accc815398
Solveyga <azbite@mail.ru>
2025-02-06 21:27:31 +0300

    another solution of 383

--- byLeetCode/383_ransom_note.py
+++ byLeetCode/383_ransom_note.py
@@ -1,13 +1,29 @@
-def canConstruct(ransomNote: str, magazine: str) -> bool:
-    compare_dict: dict = {}
+"""
+https://leetcode.com/problems/ransom-note/solutions/5821388/video-counting-each-character-2-solutions
+"""
 
+
+def canConstruct(ransomNote: str, magazine: str) -> bool:
+    magazine_dict = {}
+
+    for c in magazine:
+        magazine_dict[c] = 1 + magazine_dict.get(c, 0)
+
+    for c in ransomNote:
+        if c not in magazine_dict or magazine_dict[c] <= 0:
+            return False
+        magazine_dict[c] -= 1
+
+    return True
+
+"""
     for letter in ransomNote:
         compare_dict.setdefault(letter, 0)
         compare_dict[letter] += 1
 
     for letter in magazine:
         if letter not in compare_dict:
-            return False
+            continue
         else:
             compare_dict[letter] -= 1
 
@@ -16,9 +32,8 @@ def canConstruct(ransomNote: str, magazine: str) -> bool:
             return False
 
     return True
+    """
 
-
-print(canConstruct("abcd", "cdbe"))
 
 """
 Позитивные тесты
@@ -36,6 +51,9 @@ assert canConstruct("n", "n") == True
 """Тест проверяет случай, когда во второй строке недостаточно букв."""
 assert canConstruct("abcdr", "cadb") == False
 
+"""Тест проверяет случай, когда во второй строке больше букв, в том числе отличный от первой строки."""
+assert canConstruct("aa", "aab") == True
+
 """Тест проверяет длинные строки."""
 assert canConstruct("k" * 10000, "k" * 10000) == True
 
@@ -45,7 +63,7 @@ assert canConstruct("k" * 10000, "k" * 10000) == True
 """
 
 """Тест для пустой строки"""
-assert canConstruct("", "ft") == False
+assert canConstruct("", "ft") == True
 
 """Тест с одной буквой в разном регистре. Обработка ошибки нет, поэтому функция верент валидный результат."""
 assert canConstruct("n", "N") == False
